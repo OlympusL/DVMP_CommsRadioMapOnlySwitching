12e865c96f838035ec8137b775575c41c17d50c6
Olympus9155 <34935565+OlympusL@users.noreply.github.com>
2026-08-17 12:57:09 +0800

    Initial commit

---
+++ DVMP_CommsRadioMapOnlySwitching/HarmonyPatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+
+
+namespace DVMP_CommsRadioMapOnlySwitching
+{
+    [HarmonyPatch(typeof(JunctionSwitcherManager), "UpdateJunctionControl")]
+    static class JunctionSwitcherManager_UpdateJunctionControl_Patch
+    {
+        static bool Prefix(
+            JunctionSwitcher switcher,
+            JunctionSwitchRemoteControllable junctionControl,
+            bool indirectlyPointing
+        )
+        {
+            if (switcher is JunctionSwitcher && !indirectlyPointing) return false;
+            return true;
+        }
+    }
+}
---
+++ DVMP_CommsRadioMapOnlySwitching/Main.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using UnityModManagerNet;
+
+namespace DVMP_CommsRadioMapOnlySwitching
+{
+    [EnableReloading]
+    public static class Main
+    {
+        public static UnityModManager.ModEntry? mod;
+
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            mod = modEntry;
+            mod.OnToggle = OnToggle;
+
+            return true;
+        }
+
+        private static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            Harmony harmony = new Harmony(modEntry.Info.Id);
+
+            if (value)
+            {
+                harmony.PatchAll();
+            }
+            else
+            {
+                harmony.UnpatchAll(modEntry.Info.Id);
+            }
+            return true;
+        }
+    }
+}
